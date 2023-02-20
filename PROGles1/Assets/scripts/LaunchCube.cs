6af693e2b06ce93745bbe02b782f75d984ec4f19
Timothy Kuijper <32886@ma-web.nl>
2023-02-20 14:31:42 +0100

    lets try this shit again

---
+++ PROGles1/Assets/scripts/LaunchCube.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchCube : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    public float m_Thrust = 20f;
+
+    void Start()
+    {  
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            m_Rigidbody.AddForce(transform.up * m_Thrust);
+        }
+    }
+}
+
